f683b5ba1276e85c00bb3db7a96dc9fb3e8ada89
Sumit Chavan <sdchavan8070@gmail.com>
2025-06-26 19:24:12 +0530

    backend url updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -69,7 +69,7 @@ interface TestSuite {
   displayName: string;
 }
 
-export const BACKEND_URL = "http://43.204.98.173:5000";
+export const BACKEND_URL = "http://13.203.224.56:5000";
 // export const BACKEND_URL = "http://localhost:5000";
 
 export default function AutomationPortfolio() {
